a54ddcf4c5108879ae2a4623efbf1f2ac1e1dc2c
GeaugaJack <53152942+GeaugaJack@users.noreply.github.com>
2019-07-21 15:13:42 -0400

    Add files via upload

---
+++ Correct Movie Format.py
@@ -0,0 +1,320 @@
+import random
+
+min = 1
+max = 100
+
+roll_again = ("y")
+
+
+while roll_again == "yes" or roll_again == "y":
+    print("Jack has not seen..")
+    break
+result = (random.randint(min, max))
+
+if result == 1:
+    print("The Matrix Series")
+
+if result == 2:
+    print("Termiantor")
+
+if result == 3:
+    print("Pirates of the Carribean")
+
+if result == 4:
+    print("The Original Star Wars Movies")
+
+if result == 5:
+    print("Super Troopers")
+
+if result == 6:
+    print("Any spider-man movie")
+
+if result == 7:
+    print("The Lord Of The Rings Trilogy")
+
+if result == 8:
+    print("Any Batman Movie")
+
+if result == 9:
+    print("The Harry Potter Series")
+
+if result == 10:
+    print("Any Fast & The Furious Movie")
+
+if result == 11:
+    print("The GodFather Series")
+
+if result == 12:
+    print("Titantic")
+
+if result == 13:
+    print("Any Rocky Movie")
+
+if result == 14:
+    print("The Back To The Future Trilogy")
+
+if result == 15:
+    print("The Lethal Weapon Series")
+
+if result == 16:
+    print("The Hangover Trilogy")
+
+if result == 17:
+    print("Any Rambo Movie")
+
+if result == 18:
+    print("Any Die Hard Movie")
+
+if result == 19:
+    print("The Lion King")
+
+if result == 20:
+    print("Frozen")
+
+if result == 21:
+    print("Aladdin")
+
+if result == 22:
+    print("Any Saw Movie")
+
+if result == 23:
+    print("Any Halloween Movie")
+
+if result == 23:
+    print("Any Friday The 13th Movie")
+    
+if result == 24:
+    print("Up")
+
+if result == 24:
+    print("Bambi")
+
+if result == 25:
+    print("Dumbo")
+
+if result == 26:
+    print("50 Shades Of Gray")
+
+if result == 27:
+    print("Pinocchio")
+
+if result == 28:
+    print("Alice In Wonderland")
+
+if result == 29:
+    print("The Jungle Book")
+
+if result == 30:
+    print("Raiders of the Lost Ark")
+
+if result == 31:
+    print("Any Top Gun Movie")
+
+if result == 32:
+    print("Any Karate Kid Movie")
+
+if result == 33:
+    print("Caddyshack")
+
+if result == 34:
+    print("A Nightmare on Elm Street")
+
+if result == 35:
+    print("Footloose")
+
+if result == 36:
+    print("Who Framed Roger Rabbit")
+
+if result == 37:
+    print("Pulp Fiction")
+
+if result == 38:
+    print("The Original Jurassic Park Series")
+
+if result == 39:
+    print("Fight Club")
+
+if result == 40:
+    print("Seven")
+
+if result == 41:
+    print("The Home Alone Series")
+
+if result == 42:
+    print("The Hitchhikers Guide To The Galaxy")
+
+if result == 43:
+    print("Goodfellas")
+
+if result == 44:
+    print("Scream")
+
+if result == 45:
+    print("Waynes World")
+
+if result == 46:
+    print("Reservoir Dogs")
+
+if result == 47:
+    print("Mrs. Doubtfire")
+
+if result == 48:
+    print("Bad News Bears")
+
+if result == 49:
+    print("Billy Madison")
+
+if result == 50:
+    print("Office Space")
+
+if result == 51:
+    print("Mean Girls")
+
+if result == 52:
+    print("White Chicks")
+
+if result == 53:
+    print("Dont be a Menace")
+
+if result == 54:
+    print("X-Men")
+
+if result == 55:
+    print("The Incredibel Hulk")
+
+if result == 56:
+    print("Finding Dory")
+
+if result == 57:
+    print("Inglorious Bastards")
+
+if result == 58:
+    print("300")
+
+if result == 59:
+    print("A Bugs Life")
+
+if result == 60:
+    print("Any Diary of a Whimpy Kid Movie")
+
+if result == 61:
+    print("Toy Story 2")
+
+if result == 62:
+    print("Lilo & Stitch")
+
+if result == 63:
+    print("Any High School Musical Movie")
+
+if result == 64:
+    print("Wreck it Ralph")
+
+if result == 65:
+    print("Any Mission Impossible Movie")
+
+if result == 66:
+    print("Any Bourne Movie")
+
+if result == 67:
+    print("Moana")
+
+if result == 68:
+    print("Stuart Little")
+
+if result == 69:
+    print("Old Yeller")
+
+if result == 70:
+    print("Step Brothers")
+
+if result == 71:
+    print("Inception")
+
+if result == 72:
+    print("The Wolf of Wall Street")
+
+if result == 73:
+    print("Avatar The Last Airbender Movie")
+
+if result == 74:
+    print("Tarzan")
+
+if result == 75:
+    print("Tangled")
+
+if result == 76:
+    print("Interstellar")
+
+if result == 77:
+    print("Logan")
+
+if result == 78:
+    print("The Longest Yard")
+
+if result == 79:
+    print("The Wedding Singer")
+
+if result == 80:
+    print("Tropic Thunder")
+
+if result == 81:
+    print("The Great Gatsby")
+
+if result == 82:
+    print("American Graffiti")
+
+if result == 83:
+    print("Grease")
+
+if result == 84:
+    print("Monty Python's Life of Brian")
+
+if result == 85:
+    print("The Good, The Bad, and The Ugly")
+
+if result == 86:
+    print("The Exorcist")
+
+if result == 87:
+    print("Dirty Harry")
+
+if result == 88:
+    print("The Ring")
+
+if result == 89:
+    print("The Child Play's Series")
+
+if result == 90:
+    print("Jaws 2-4")
+
+if result == 91:
+    print("Purple Rain")
+
+if result == 92:
+    print("Any Final Destination Movie")
+
+if result == 93:
+    print("Poltergeist")
+
+if result == 94:
+    print("Psycho")
+
+if result == 95:
+    print("The Shining")
+
+if result == 96:
+    print("The Mist")
+
+if result == 97:
+    print("8 Mile")
+
+if result == 98:
+    print("Seven")
+
+if result == 99:
+    print("The Bill & Ted Series")
+
+if result == 100:
+    print("Any John Wick Movie")
+
+roll_again = input("Roll the Dice Again?")
